7dce5ec453a82f2ad98415a5cea11afae9205148
Sasha <georgefbrockman@gmail.com>
2024-01-26 09:34:53 -0600

    finishes lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        """The title property"""
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        """Title setter function"""
+        self._title = title
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        """page_count must be an integer"""
+        if(type(page_count) == int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        """The brand property"""
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        """The brand setter"""
+        self._brand = brand
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """size must be an integer"""
+        if(type(size) == int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        """The condition property"""
+        return self._condition
+
+    @condition.setter
+    def condition(self, condition):
+        """The condition setter"""
+        self._condition = condition
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
